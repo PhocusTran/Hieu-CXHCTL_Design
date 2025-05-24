49152bdc774db955a40c0e50e560289675788b4c
Trần Trung Hiếu <87080535+HieuTran2002@users.noreply.github.com>
2025-05-24 15:49:09 +0700

    Delete esp_file_server_usb_msc_sdmmc directory
    rm

--- esp_file_server_usb_msc_sdmmc/main/http_server.h
+++
@@ -1,8 +0,0 @@
-#ifndef HTTP_SERVER_H
-#define HTTP_SERVER_H
-
-#include "esp_err.h"
-
-esp_err_t wifi_init(const char* ssid, const char* pwd);
-
-#endif
--- esp_file_server_usb_msc_sdmmc/main/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "http_server.h"
-
-void app_main(void)
-{
-    wifi_init("HCTL", "123456789HCTL");
-}
